1585fb5d47b9e04b769b8c8e83ac96431d19b6f7
wingled22 <pelayowindel@gmail.com>
2024-03-08 17:20:00 +0800

    ...

---
+++ Entities/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web2BMVC.Entities;
+
+public partial class Animal
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
